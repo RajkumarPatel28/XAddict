35a675e3d93e60a34b30e6a57dadd733c41abfe8
Sharmaa-amit <71359389+Sharmaa-amit@users.noreply.github.com>
2020-10-04 14:44:43 -0400

    Splash Screen Activity
    Created the activity, prepping for layout

--- XAddict/src/main/java/com/reduce_abuse/xaddict/SplashScreenActivity.java
+++ XAddict/src/main/java/com/reduce_abuse/xaddict/SplashScreenActivity.java
@@ -1,18 +1,14 @@
 package com.reduce_abuse.xaddict;
-// Commit by Amit Sharma
-// Commint by meet
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
-// muneeb was testing git
-
-// Raj was testing Git
