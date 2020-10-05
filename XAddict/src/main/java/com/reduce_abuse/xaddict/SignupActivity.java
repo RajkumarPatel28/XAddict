2f8d8c395b644a43ed748d0a41a103ed12673f5e
Sharmaa-amit <71359389+Sharmaa-amit@users.noreply.github.com>
2020-10-04 21:09:12 -0400

    Signup Activity
    Added some functionality to buttons

--- XAddict/src/main/java/com/reduce_abuse/xaddict/SignupActivity.java
+++ XAddict/src/main/java/com/reduce_abuse/xaddict/SignupActivity.java
@@ -2,13 +2,42 @@ package com.reduce_abuse.xaddict;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_signup);
+
+
+       Button btnSignup = (Button) findViewById(R.id.btnSignup);
+       Button btnProceedLogin = (Button) findViewById(R.id.btnProceedLogin);
+
+       btnSignup.setOnClickListener(this);
+       btnProceedLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = null;
+
+        switch(v.getId())
+        {
+            case R.id.btnSignup:
+                // Add logic
+                break;
+            case R.id.btnProceedLogin:
+                intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+
     }
 }
