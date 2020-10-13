1cdaafe43b283fa3c103209144641410fc4bb1be
meetp752000 <71569357+meetp752000@users.noreply.github.com>
2020-10-13 17:55:28 -0400

    DrugList NOT COMPLETE
    Added Adapter and recycle view
    NOT DONE YET THO

--- XAddict/src/main/java/com/reduce_abuse/xaddict/DrugsListActivity.java
+++ XAddict/src/main/java/com/reduce_abuse/xaddict/DrugsListActivity.java
@@ -1,15 +1,28 @@
 package com.reduce_abuse.xaddict;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class DrugsListActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
+    String DrugName[], DrugDescripition[];
+    int Images [] ={R.drawable.cocaine, R.drawable.herion, R.drawable.marijuana, R.drawable.mdma, R.drawable.meth, R.drawable.opiodios};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drugs_list);
-        
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        DrugName = getResources().getStringArray(R.array.DrugsList_DrugName);
+        DrugDescripition = getResources().getStringArray(R.array.DrugsList_DrugDescripition);
+
+        AdapterDrugList AdapterDrugList = new AdapterDrugList(this,DrugName, DrugDescripition, Images);
+
     }
 }
