d5072496120bdf773e9c3ca3a122138a0185624d
meetp752000 <71569357+meetp752000@users.noreply.github.com>
2020-10-13 15:56:19 -0400

    change image splash
    splash

--- XAddict/src/main/java/com/reduce_abuse/xaddict/DrugsListActivity.java
+++ XAddict/src/main/java/com/reduce_abuse/xaddict/DrugsListActivity.java
@@ -10,5 +10,6 @@ public class DrugsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drugs_list);
+        
     }
 }
