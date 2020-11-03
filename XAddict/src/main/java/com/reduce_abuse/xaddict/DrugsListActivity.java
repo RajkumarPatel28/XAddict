2daef03f7363938cf102be12f83375480b77bcf8
meetp752000 <71569357+meetp752000@users.noreply.github.com>
2020-11-03 14:00:50 -0500

    code format

--- XAddict/src/main/java/com/reduce_abuse/xaddict/DrugsListActivity.java
+++ XAddict/src/main/java/com/reduce_abuse/xaddict/DrugsListActivity.java
@@ -1,10 +1,10 @@
 package com.reduce_abuse.xaddict;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class DrugsListActivity extends AppCompatActivity {
 
