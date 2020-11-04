9f6f6550018b5d2a8c3d1f1d8d2b018d7a5f27b3
RajkumarPatel28 <RajkumarPatel28@users.noreply.github.com>
2020-11-04 03:57:25 -0500

    Login Activity
    Added a textview with having onclick to reset the user's password as it connects to the firebase

--- XAddict/src/main/java/com/reduceabuse/xaddict/LoginActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.reduceabuse.xaddict;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,9 +12,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,6 +25,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText etLoginEmailAddress, etLoginPassword;
+    TextView tvLoginForgotPassword;
     FirebaseAuth fbAuth;
     ProgressBar pblogin;
 
@@ -30,18 +36,49 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         etLoginEmailAddress = findViewById(R.id.etLoginEmailAddress);
         etLoginPassword = findViewById(R.id.etLoginPassword);
+        tvLoginForgotPassword = (TextView) findViewById(R.id.tvLoginForgotPassword);
 
         fbAuth = FirebaseAuth.getInstance();
         pblogin = findViewById(R.id.pblogin);
 
         Button btnLogin = findViewById(R.id.btnLogin);
         Button btnLoginProceedSignup = findViewById(R.id.btnLoginProceedSignup);
-        Button btnLoginForgotPassword = findViewById(R.id.btnLoginForgotPassword);
 
         btnLogin.setOnClickListener(this);
         btnLoginProceedSignup.setOnClickListener(this);
-        btnLoginForgotPassword.setOnClickListener(this);
+    }
 
+    public void passwordFunction(View v){
+
+        final EditText resetPassword = new EditText(v.getContext());
+        AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
+        dialog.setMessage("Enter your email to receive a link that will reset your password.");
+        dialog.setView(resetPassword);
+
+        dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            String emailAddress = resetPassword.getText().toString();
+            fbAuth.sendPasswordResetEmail(emailAddress).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(LoginActivity.this, "There is a reset link sent to your email", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Error, no reset link sent!" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        });
+        dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+
+        }
+        });
+        dialog.create().show();
     }
 
     @Override
