0c38c87e4e7f14a5ad10eee0d19d56224758e3af
RajkumarPatel28 <RajkumarPatel28@users.noreply.github.com>
2020-11-12 17:45:23 -0500

    LoginActivity
    - Finalized LoginActivity
    - Added additional error checking
    - Reformated code
    - Removed hard coded strings

--- XAddict/src/main/java/com/reduceabuse/xaddict/LoginActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/LoginActivity.java
@@ -22,111 +22,141 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Objects;
 
-    EditText etLoginEmailAddress, etLoginPassword;
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etLoginEmailAddress, etLoginPassword, etResetPassword;
     TextView tvLoginForgotPassword;
+    ProgressBar pbLogin;
     FirebaseAuth fbAuth;
-    ProgressBar pblogin;
+    String emailAddress, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         etLoginEmailAddress = findViewById(R.id.etLoginEmailAddress);
         etLoginPassword = findViewById(R.id.etLoginPassword);
-        tvLoginForgotPassword = (TextView) findViewById(R.id.tvLoginForgotPassword);
+        pbLogin = findViewById(R.id.pbLogin);
 
         fbAuth = FirebaseAuth.getInstance();
-        pblogin = findViewById(R.id.pblogin);
 
         Button btnLogin = findViewById(R.id.btnLogin);
-        Button btnLoginProceedSignup = findViewById(R.id.btnLoginProceedSignup);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-        btnLogin.setOnClickListener(this);
-        btnLoginProceedSignup.setOnClickListener(this);
-    }
+                emailAddress = etLoginEmailAddress.getText().toString().trim();
+                password = etLoginPassword.getText().toString().trim();
 
-    public void passwordFunction(View v){
-
-        final EditText resetPassword = new EditText(v.getContext());
-        AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
-        dialog.setMessage("Enter your email to receive a link that will reset your password.");
-        dialog.setView(resetPassword);
-
-        dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            String emailAddress = resetPassword.getText().toString();
-            fbAuth.sendPasswordResetEmail(emailAddress).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(LoginActivity.this, "There is a reset link sent to your email", Toast.LENGTH_SHORT).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(LoginActivity.this, "Error, no reset link sent!" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        });
-        dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-
-        }
-        });
-        dialog.create().show();
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        String emailAddress, password;
-
-        emailAddress = etLoginEmailAddress.getText().toString().trim();
-        password = etLoginPassword.getText().toString().trim();
-
-        switch(v.getId()) {
-            case R.id.btnLogin:
                 if (TextUtils.isEmpty(emailAddress)) {
-                    etLoginEmailAddress.setError("Please enter an email address");
+                    etLoginEmailAddress.setError(getString(R.string.login_emailerror));
                     return;
                 }
 
                 if (TextUtils.isEmpty(password)) {
-                    etLoginPassword.setError("Please enter a password");
+                    etLoginPassword.setError(getString(R.string.login_passworderror));
                     return;
                 }
 
-                if (password.length() < 6) {
-                    etLoginPassword.setError("Password must be 6 or more characters");
-                    return;
-                }
-
-                pblogin.setVisibility(View.VISIBLE);
+                pbLogin.setVisibility(View.VISIBLE);
 
                 fbAuth.signInWithEmailAndPassword(emailAddress, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(LoginActivity.this, HomepageActivity.class);
                             startActivity(intent);
                         } else {
-                            Toast.makeText(LoginActivity.this, "Error to login!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            pblogin.setVisibility(View.GONE);
+                            Toast.makeText(LoginActivity.this, getString(R.string.login_loginerror), Toast.LENGTH_SHORT).show();
                         }
+                        pbLogin.setVisibility(View.INVISIBLE);
                     }
                 });
-                break;
+            }
+        });
 
-            case R.id.btnLoginProceedSignup:
+        Button btnLoginProceedSignup = findViewById(R.id.btnLoginProceedSignup);
+        btnLoginProceedSignup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);
-                break;
-        }
+            }
+        });
+
+        tvLoginForgotPassword = findViewById(R.id.tvLoginForgotPassword);
+        tvLoginForgotPassword.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+
+                etResetPassword = new EditText(v.getContext());
+                emailAddress = etLoginEmailAddress.getText().toString().trim();
+                etResetPassword.setText(emailAddress);
+
+                builder.setMessage(getString(R.string.login_dialogresetmessage))
+                        .setView(etResetPassword)
+                        .setCancelable(false)
+                        .setPositiveButton(getString(R.string.login_dialogpositive), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                emailAddress = etResetPassword.getText().toString().trim();
+
+                                if (TextUtils.isEmpty(emailAddress)) {
+                                    Toast.makeText(LoginActivity.this, getString(R.string.login_dialogresetfailuremessage), Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+                                fbAuth.sendPasswordResetEmail(emailAddress).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(LoginActivity.this, getString(R.string.login_dialogresetsuccessmessage), Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, getString(R.string.login_dialogresetfailuremessage), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton(getString(R.string.login_dialognegative), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+
+                AlertDialog dialog = builder.create();
+                dialog.setTitle(getString(R.string.login_dialogresettitle));
+                dialog.setIcon(android.R.drawable.ic_dialog_email);
+                dialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.login_dialogbackmessage))
+                .setCancelable(false)
+                .setPositiveButton(getString(R.string.login_dialogpositive), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        LoginActivity.this.finish();
+                    }
+                })
+                .setNegativeButton(getString(R.string.login_dialognegative), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.setTitle(getString(R.string.login_dialogbacktitle));
+        dialog.setIcon(android.R.drawable.ic_dialog_alert);
+        dialog.show();
     }
 }
