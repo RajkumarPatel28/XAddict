836860d0e865c40ea36e3054be1d9e74a33870b1
RajkumarPatel28 <RajkumarPatel28@users.noreply.github.com>
2020-11-12 11:25:12 -0500

    Settings Activity
    -Changed style of text and added strings

--- XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
@@ -32,13 +32,21 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.account:
-                setTitle(getString(R.string.account_title));
+            case R.id.profile:
+                setTitle(getString(R.string.profile_title));
                 settingsVf.setDisplayedChild(1);
                 return true;
-            case R.id.privacy:
-                setTitle(getString(R.string.privacy_title));
+            case R.id.theme:
+                setTitle(getString(R.string.theme_title));
                 settingsVf.setDisplayedChild(2);
+                return true;
+            case R.id.aboutUs:
+                setTitle(getString(R.string.aboutus_title));
+                settingsVf.setDisplayedChild(3);
+                return true;
+            case R.id.appFeedback:
+                setTitle(getString(R.string.appfeedback_title));
+                settingsVf.setDisplayedChild(4);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
