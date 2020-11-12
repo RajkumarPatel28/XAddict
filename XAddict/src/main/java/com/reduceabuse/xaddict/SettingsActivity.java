889310ae9386f32d13c9b5093c0db5b42c8b65ff
RajkumarPatel28 <RajkumarPatel28@users.noreply.github.com>
2020-11-12 00:25:30 -0500

    Settings Activity
    -Added an overflow for the settings activity
    -Added 4 items in the overflow which are (Account. Privacy, About, and App Feedback)

--- XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
@@ -3,12 +3,45 @@ package com.reduceabuse.xaddict;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ViewFlipper;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    ViewFlipper settingsVf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setTitle("Setting");
+
+        settingsVf = findViewById(R.id.settingsVf);
+        settingsVf.setDisplayedChild(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settingsoverflow, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.account:
+                setTitle(getString(R.string.account_title));
+                settingsVf.setDisplayedChild(1);
+                return true;
+            case R.id.privacy:
+                setTitle(getString(R.string.privacy_title));
+                settingsVf.setDisplayedChild(2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
