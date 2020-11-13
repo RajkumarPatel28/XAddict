57169951e2f1c117a9dd7f0af982acdb131d48ac
MuneebAhmed00 <m.madrid.ahmed@gmail.com>
2020-11-13 12:36:12 -0500

    modified nav bar
    changed the nav bar to match with the new images and renamed the menu.xml

--- XAddict/src/main/java/com/reduceabuse/xaddict/HomepageActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/HomepageActivity.java
@@ -1,43 +1,49 @@
 package com.reduceabuse.xaddict;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Button;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomepageActivity extends AppCompatActivity{
+import java.util.Objects;
+
+public class HomepageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                Intent intent;
+                switch (item.getItemId()) {
 
-                    case R.id.homeBtn:
+                    case R.id.btnDrugs:
+                        Intent intent1 = new Intent(HomepageActivity.this, DrugsListActivity.class);
+                        startActivity(intent1);
                         break;
 
-                    case R.id.drugBtn:
-                        Intent intent2 = new Intent(HomepageActivity.this, DrugsListActivity.class);
+                    case R.id.btnSchedule:
+                        Intent intent2 = new Intent(HomepageActivity.this, PersonalNotesActivity.class);
                         startActivity(intent2);
                         break;
 
-                    case R.id.notesBtn:
+                    case R.id.btnStatus:
                         Intent intent3 = new Intent(HomepageActivity.this, PersonalNotesActivity.class);
                         startActivity(intent3);
                         break;
 
-                    case R.id.settingsBtn:
+
+                    case R.id.btnSettings:
                         Intent intent4 = new Intent(HomepageActivity.this, SettingsActivity.class);
                         startActivity(intent4);
                         break;
@@ -46,12 +52,5 @@ public class HomepageActivity extends AppCompatActivity{
             }
 
         });
-
-
     }
-
-
-
-
-    }
-
+}
