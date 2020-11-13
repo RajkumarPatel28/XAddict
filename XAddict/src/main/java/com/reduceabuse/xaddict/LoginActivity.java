1a3906e74f98da7ff2ed639c4dbb289e2637cbb8
RajkumarPatel28 <n01289472@humbermail.ca>
2020-11-13 11:43:55 -0500

    LoginActivity
    -Added minor changes

--- XAddict/src/main/java/com/reduceabuse/xaddict/LoginActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/LoginActivity.java
@@ -145,7 +145,7 @@ public class LoginActivity extends AppCompatActivity {
                 .setPositiveButton(getString(R.string.login_dialogpositive), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        LoginActivity.this.finish();
+                        System.exit(0);
                     }
                 })
                 .setNegativeButton(getString(R.string.login_dialognegative), new DialogInterface.OnClickListener() {
