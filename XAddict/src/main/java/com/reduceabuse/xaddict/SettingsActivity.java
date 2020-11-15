9c33446ce62d7bc8123e08ca205d0a02c8750b7c
RajkumarPatel28 <n01289472@humbermail.ca>
2020-11-14 20:58:52 -0500

    SettingsActivity
    - Added SharedPreferences to save users Name and Phone Number in SettingsActivity
    - Changed strings to prevent hard code strings

--- XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
@@ -1,17 +1,27 @@
 package com.reduceabuse.xaddict;
 
+import androidx.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class SettingsActivity extends AppCompatActivity {
 
     ViewFlipper vfSettings;
+    EditText etFullName, etPhoneNumber;
+    Button btnProfileSubmit, btnLogout;
+    String fullName, phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,48 @@ public class SettingsActivity extends AppCompatActivity {
 
         vfSettings = findViewById(R.id.vfSettings);
         vfSettings.setDisplayedChild(0);
+
+        etFullName = findViewById(R.id.etFullName);
+        etPhoneNumber = findViewById(R.id.etPhoneNumber);
+        btnProfileSubmit = findViewById(R.id.btnProfileSubmit);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        SharedPreferences saveData = PreferenceManager.getDefaultSharedPreferences(this);
+        String fullName1 = saveData.getString(getString(R.string.settings_fullnamevalue),"");
+        String phoneNumber1 = saveData.getString(getString(R.string.settings_phonenumbervalue),"");
+        etFullName.setText(fullName1);
+        etPhoneNumber.setText(phoneNumber1);
+
+        btnProfileSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fullName = etFullName.getText().toString();
+                phoneNumber = etPhoneNumber.getText().toString();
+
+                SharedPreferences saveData = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+                SharedPreferences.Editor editor = saveData.edit();
+                editor.putString(getString(R.string.settings_fullnamevalue),fullName);
+                editor.putString(getString(R.string.settings_phonenumbervalue),phoneNumber);
+                editor.apply();
+
+                if (TextUtils.isEmpty(fullName)) {
+                    Toast.makeText(SettingsActivity.this, getString(R.string.settings_fullnameerror), Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(SettingsActivity.this, getString(R.string.settings_updatedmessage), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SettingsActivity.this, getString(R.string.settings_logoutmessage), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
