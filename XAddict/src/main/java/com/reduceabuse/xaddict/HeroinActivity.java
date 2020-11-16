d4d8a575640e05f93b9a841f73c9b853f2b38954
RajkumarPatel28 <n01289472@humbermail.ca>
2020-11-15 23:40:05 -0500

    ActivityHeroin
    - Added information for ActivityHeroin
    - Added link to websire as button is clicked
    - Added landscape for ActivityHeroin

--- XAddict/src/main/java/com/reduceabuse/xaddict/HeroinActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/HeroinActivity.java
@@ -2,7 +2,11 @@ package com.reduceabuse.xaddict;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
@@ -14,5 +18,14 @@ public class HeroinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_heroin);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        Button btnInfo = findViewById(R.id.btnInfo);
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.canada.ca/en/health-canada/services/substance-use/controlled-illegal-drugs/heroin.html"));
+                startActivity(intent);
+            }
+        });
     }
 }
