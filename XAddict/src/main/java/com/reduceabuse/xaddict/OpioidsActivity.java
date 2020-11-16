92750f74486dc2f0d6e965c59af52e831bc6f581
meetp752000 <71569357+meetp752000@users.noreply.github.com>
2020-11-16 00:09:35 -0500

    Added onclick for the button links

--- XAddict/src/main/java/com/reduceabuse/xaddict/OpioidsActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/OpioidsActivity.java
@@ -2,7 +2,11 @@ package com.reduceabuse.xaddict;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
@@ -13,6 +17,16 @@ public class OpioidsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opioids);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        Button btnInfo = findViewById(R.id.btnInfo);
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.canada.ca/en/health-canada/services/substance-use/problematic-prescription-drug-use/opioids/about.html"));
+                startActivity(intent);
+            }
+        });
 
     }
 }
