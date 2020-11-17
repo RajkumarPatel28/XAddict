03e61c9d09c8b3cf8ed1fae2526ed7993f7d4c04
RajkumarPatel28 <n01289472@humbermail.ca>
2020-11-17 01:38:02 -0500

    SettingsActivity
    - Updated settings for feedback to link onto our firebase

--- XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
@@ -1,12 +1,9 @@
 package com.reduceabuse.xaddict;
 
-import androidx.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,18 +14,21 @@ import android.widget.RatingBar;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
 
     ViewFlipper vfSettings;
-    EditText etFullName, etPhoneNumber, etName, etReview;
+    EditText etFullName, etPhoneNumber, etReview;
     Button btnProfileSubmit, btnLogout, btnFeedbackSubmit;
-    String fullName, phoneNumber, data, rating, feedback;
-    RatingBar rbFeedback;
-    FirebaseDatabase database;
-    DatabaseReference ref;
+    String fullName, phoneNumber, rating, review, userID;
+    RatingBar rbRating;
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,37 +43,17 @@ public class SettingsActivity extends AppCompatActivity {
         etPhoneNumber = findViewById(R.id.etPhoneNumber);
         btnProfileSubmit = findViewById(R.id.btnProfileSubmit);
         btnLogout = findViewById(R.id.btnLogout);
-        etName = findViewById(R.id.etName);
         etReview = findViewById(R.id.etReview);
-        rbFeedback = findViewById(R.id.rbFeedback);
+        rbRating = findViewById(R.id.rbRating);
         btnFeedbackSubmit = findViewById(R.id.btnFeedbackSubmit);
 
-        SharedPreferences saveData = PreferenceManager.getDefaultSharedPreferences(this);
-        String fullName1 = saveData.getString(getString(R.string.settings_fullnamevalue), "");
-        String phoneNumber1 = saveData.getString(getString(R.string.settings_phonenumbervalue), "");
-        etFullName.setText(fullName1);
-        etPhoneNumber.setText(phoneNumber1);
-
         database = FirebaseDatabase.getInstance();
+        userID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
 
         btnProfileSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                fullName = etFullName.getText().toString();
-                phoneNumber = etPhoneNumber.getText().toString();
-
-                SharedPreferences saveData = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
-                SharedPreferences.Editor editor = saveData.edit();
-                editor.putString(getString(R.string.settings_fullnamevalue), fullName);
-                editor.putString(getString(R.string.settings_phonenumbervalue), phoneNumber);
-                editor.apply();
-
-                if (TextUtils.isEmpty(fullName)) {
-                    Toast.makeText(SettingsActivity.this, getString(R.string.settings_fullnameerror), Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(SettingsActivity.this, getString(R.string.settings_updatedmessage), Toast.LENGTH_SHORT).show();
-                }
             }
         });
 
@@ -89,23 +69,21 @@ public class SettingsActivity extends AppCompatActivity {
         btnFeedbackSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data = etName.getText().toString();
-                ref = database.getReference(getString(R.string.settings_reviewstitle)).child(data);
-                rating = String.valueOf(rbFeedback.getRating());
-                feedback = etReview.getText().toString();
-
-                ref.child(getString(R.string.settings_ratingtitle)).setValue(rating);
-                ref.child(getString(R.string.settings_feedbacktitle)).setValue(feedback);
-
-                if (TextUtils.isEmpty(feedback)) {
-                    Toast.makeText(SettingsActivity.this, getString(R.string.settings_feedbackerror), Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(SettingsActivity.this, getString(R.string.settings_feedbackmessage), Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(SettingsActivity.this, HomepageActivity.class);
-                    startActivity(intent);
-                }
+                saveReview();
+                Toast.makeText(SettingsActivity.this, getString(R.string.settings_feedbackmessage), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SettingsActivity.this, SettingsActivity.class);
+                startActivity(intent);
             }
         });
+    }
+
+    public void saveReview() {
+        ref = database.getReference(getString(R.string.settings_reviewstitle)).child(userID);
+        rating = String.valueOf(rbRating.getRating());
+        review = etReview.getText().toString();
+
+        ref.child(getString(R.string.settings_ratingtitle)).setValue(rating);
+        ref.child(getString(R.string.settings_feedbacktitle)).setValue(review);
     }
 
     @Override
