e1b1739cab7a021bfc0952a0590709d1b0ad3e5e
RajkumarPatel28 <n01289472@humbermail.ca>
2020-11-17 15:07:53 -0500

    SettingsActivity
    - Updated SettingsActivity to the profile and feedback settings to connect to the firebase
    - Updated the strings to prevent hard coding

--- XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.reduceabuse.xaddict;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,8 +17,11 @@ import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
@@ -24,7 +29,6 @@ public class SettingsActivity extends AppCompatActivity {
 
     ViewFlipper vfSettings;
     EditText etFullName, etPhoneNumber, etReview;
-    Button btnProfileSubmit, btnLogout, btnFeedbackSubmit;
     String fullName, phoneNumber, rating, review, userID;
     RatingBar rbRating;
     private FirebaseDatabase database;
@@ -41,22 +45,31 @@ public class SettingsActivity extends AppCompatActivity {
 
         etFullName = findViewById(R.id.etFullName);
         etPhoneNumber = findViewById(R.id.etPhoneNumber);
-        btnProfileSubmit = findViewById(R.id.btnProfileSubmit);
-        btnLogout = findViewById(R.id.btnLogout);
+
         etReview = findViewById(R.id.etReview);
         rbRating = findViewById(R.id.rbRating);
-        btnFeedbackSubmit = findViewById(R.id.btnFeedbackSubmit);
 
         database = FirebaseDatabase.getInstance();
         userID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
 
+        loadProfile();
+
+        Button btnProfileSubmit = findViewById(R.id.btnProfileSubmit);
         btnProfileSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                phoneNumber = etPhoneNumber.getText().toString().trim();
 
+                if (TextUtils.isEmpty(phoneNumber)) {
+                    etPhoneNumber.setError(getString(R.string.settings_phonenumbererror));
+                    return;
+                }
+                saveProfile();
+                Toast.makeText(SettingsActivity.this, getString(R.string.settings_profilemessage), Toast.LENGTH_SHORT).show();
             }
         });
 
+        Button btnLogout = findViewById(R.id.btnLogout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,6 +79,7 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        Button btnFeedbackSubmit = findViewById(R.id.btnFeedbackSubmit);
         btnFeedbackSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,6 +98,36 @@ public class SettingsActivity extends AppCompatActivity {
 
         ref.child(getString(R.string.settings_ratingtitle)).setValue(rating);
         ref.child(getString(R.string.settings_feedbacktitle)).setValue(review);
+    }
+
+    public void saveProfile() {
+        ref = database.getReference(getString(R.string.settings_profiletitle)).child(userID);
+        fullName = etFullName.getText().toString();
+        phoneNumber = etPhoneNumber.getText().toString().trim();
+
+        ref.child(getString(R.string.settings_fullnamehint)).setValue(fullName);
+        ref.child(getString(R.string.settings_phonenumberhint)).setValue(phoneNumber);
+    }
+
+    public void loadProfile() {
+        ref = database.getReference().child(getString(R.string.settings_profiletitle)).child(userID);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(getString(R.string.settings_fullnamehint)).exists() && snapshot.child(getString(R.string.settings_phonenumberhint)).exists()) {
+                    fullName = snapshot.child(getString(R.string.settings_fullnamehint)).getValue().toString();
+                    phoneNumber = snapshot.child(getString(R.string.settings_phonenumberhint)).getValue().toString();
+
+                    etFullName.setText(fullName);
+                    etPhoneNumber.setText(phoneNumber);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
