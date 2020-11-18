870db793cbd92115b647eb82181576b8b1bd1eff
RajkumarPatel28 <n01289472@humbermail.ca>
2020-11-17 19:29:22 -0500

    LoginActivity
    - Fixed a bug for LoginActivity

--- XAddict/src/main/java/com/reduceabuse/xaddict/LoginActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/LoginActivity.java
@@ -149,6 +149,7 @@ public class LoginActivity extends AppCompatActivity {
                 .setPositiveButton(getString(R.string.login_dialogpositive), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        finishAffinity();
                         System.exit(0);
                     }
                 })
