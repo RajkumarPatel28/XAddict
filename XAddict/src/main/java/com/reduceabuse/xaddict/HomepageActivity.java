ca8d6e310086e14c7182f255363aa80919bce49a
Sharmaa-amit <71359389+Sharmaa-amit@users.noreply.github.com>
2020-12-08 22:20:12 -0500

    HomepageActivity Snackbar
    -Reformatted code
    -Implemented Snackbar
    -Added Firebase for authentication
    -Added conditional statements, restricting access to ScheduleActivity
    -Removed any instances of hardcoded strings, also translated them into french

--- XAddict/src/main/java/com/reduceabuse/xaddict/HomepageActivity.java
+++ XAddict/src/main/java/com/reduceabuse/xaddict/HomepageActivity.java
@@ -18,19 +18,31 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
 public class HomepageActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CALL = 1;
-
+    String userID;
+    Intent intent = null;
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        database = FirebaseDatabase.getInstance();
+        userID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
 
         Button btnCall = findViewById(R.id.btnCall);
         btnCall.setOnClickListener(new View.OnClickListener() {
@@ -45,33 +57,33 @@ public class HomepageActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent intent;
                 switch (item.getItemId()) {
                     case R.id.btnDrugs:
                         intent = new Intent(HomepageActivity.this, DrugsListActivity.class);
                         break;
                     case R.id.btnSchedule:
-                        intent = new Intent(HomepageActivity.this, ScheduleActivity.class);
+                        checkProfile();
                         break;
                     case R.id.btnStatus:
                         intent = new Intent(HomepageActivity.this, StatusActivity.class);
                         break;
                     default:
                         intent = new Intent(HomepageActivity.this, SettingsActivity.class);
+                        finishActivity(0);
                         break;
                 }
-                startActivity(intent);
+                if (intent != null)
+                    startActivity(intent);
                 return true;
             }
         });
-
     }
 
-    private void phoneCall() {
+    public void phoneCall() {
         if (ContextCompat.checkSelfPermission(HomepageActivity.this,
                 Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(HomepageActivity.this,
-                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+                    new String[]{Manifest.permission.CALL_PHONE}, 1);
         } else {
             Intent intent = new Intent(Intent.ACTION_CALL,
                     Uri.parse(getString(R.string.homepage_telephone)));
@@ -79,19 +91,40 @@ public class HomepageActivity extends AppCompatActivity {
         }
     }
 
+    public void checkProfile() {
+        ref = database.getReference().child(getString(R.string.settings_profiletitle)).child(userID);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(getString(R.string.settings_phonenumberhint)).exists()) {
+                    intent = new Intent(HomepageActivity.this, ScheduleActivity.class);
+                    startActivity(intent);
+                } else {
+                    Snackbar snackbar = Snackbar.make(findViewById(R.id.llHomepage), getString(R.string.homepage_updateprofilemessage), Snackbar.LENGTH_SHORT);
+                    snackbar.show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CALL) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (requestCode == 1)
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 phoneCall();
-            } else {
+
+            else
                 Toast.makeText(this, getString(R.string.homepage_permissiondeniedmessage), Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 
     @Override
     public void onBackPressed() {
-        Toast.makeText(HomepageActivity.this, R.string.homepage_message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(HomepageActivity.this, getString(R.string.homepage_message), Toast.LENGTH_SHORT).show();
     }
 }
